b4cabab000b4c6a470bb5014f21f7b6deeec75ec
Nitish kumar <43029769+heeranwalnitish@users.noreply.github.com>
2022-06-16 21:12:17 +0530

    Add files via upload
     Write a program that will reverse a four digit number. Also it checks whether the reverse is true

---
+++ python_proggrams/5.py
@@ -0,0 +1,4 @@
+# Write a program that will reverse a four digit number. Also it checks whether the reverse is true
+
+d = input("Enter the number ")
+print(f"The reverse of the number is : {d[::-1]}")
