1a5c79f050b4cb68095200e6785582c2c55224ce
Nitish kumar <43029769+heeranwalnitish@users.noreply.github.com>
2022-06-16 21:10:48 +0530

    Add files via upload
    User will input (2 nummbers). Write a program to swap the number

---
+++ python_proggrams/3.py
@@ -0,0 +1,5 @@
+# User will input (2 nummbers). Write a program to swap the number
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+
+print(f"first number is {b} and second number is {a}")
