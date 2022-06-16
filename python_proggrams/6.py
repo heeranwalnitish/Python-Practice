0ea9ffff1414f2c45e2fc57e960145c96dec624d
Nitish kumar <43029769+heeranwalnitish@users.noreply.github.com>
2022-06-16 21:12:51 +0530

    Add files via upload
    Write a program that will tell whether the number entered by the user is odd or not.

---
+++ python_proggrams/6.py
@@ -0,0 +1,8 @@
+# Write a program that will tell whether the number entered by the user is odd or not.
+num = int(input("Enter the number : "))
+
+if num%2 == 0 :
+    print(f"{num} is an even number")
+
+else:
+    print(f"{num} is a odd number")
