79af64b8b2ebb4893b3ba0cba62d34c36cfeaf72
Nitish kumar <43029769+heeranwalnitish@users.noreply.github.com>
2022-09-02 13:45:59 +0530

    Add files via upload
    Write a program that will swap numbers.

---
+++ python_proggrams/21.py
@@ -0,0 +1,7 @@
+# Write a program that will swap numbers.
+
+a = int(input("Enter first number : "))
+b = int(input("Enter the second number : "))
+
+print(f"The first number is {b}")
+print(f"Second number is {a}")
